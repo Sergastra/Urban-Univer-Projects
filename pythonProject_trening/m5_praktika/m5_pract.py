45b8b6e0f77d7645cb783244fb048e0f977f4bdb
Sergey <sergastrah@yandex.ru>
2024-06-12 11:28:32 +0400

    м5 практика 2

--- pythonProject_trening/m5_praktika/m5_pract.py
+++ pythonProject_trening/m5_praktika/m5_pract.py
@@ -15,5 +15,10 @@ class User:
 
 if __name__ == '__main__':
     database = Database()
-    user = User(input("Введите логин: "), input("Введите пароль: "), input("Повторите пароль: "))
-    database.add_user(user.username, user.password)
+    while True:
+        choise = input("Приветствую! Выберите действие: \n1 - Вход\n2 - Регистрация\n")
+        user = User(input("Введите логин: "), password := input("Введите пароль: "),
+                    password2 := input("Повторите пароль: "))
+        if password != password2:
+            exit()
+        database.add_user(user.username, user.password)
