9f2f5a68e9fd7c1b4772b6273d48997d48dbd2b0
Sergey <sergastrah@yandex.ru>
2024-06-28 16:53:05 +0400

    module6_3.1

--- module6/module6_3.py
+++ module6/module6_3.py
@@ -13,15 +13,14 @@ class Nissan(Car, Vechile):
     vehicle_type = 'sedan'
     price = 2400000
 
-    def horse_powers(self, **kwargs):
-        self.horsePower = 450
+    def horse_powers(self, horsePower):
+        super().horse_powers(horsePower)
 
-        return f'количество лошидиных сил {self.horsePower}'
+        return f'количество лошидиных сил {horsePower}'
 
 
 if __name__ == '__main__':
 
     print(Nissan.mro())
     nis = Nissan()
-    print(nis.vehicle_type, nis.price, nis.horse_powers(), sep='\n')
-
+    print(nis.vehicle_type, nis.price, nis.horse_powers(450), sep='\n')
