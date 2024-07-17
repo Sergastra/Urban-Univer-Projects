4c96ad254bb7add7893b006d6e67b28caa98fc52
Sergey <sergastrah@yandex.ru>
2024-07-17 22:41:35 +0400

    7_4 file1

--- module7_4.py
+++ module7_4.py
@@ -5,11 +5,11 @@ directory = '.'
 
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(file)
-        filetime = os.path.getmtime(file)
-        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime())
-        file_size = os.path.getsize(file)
-        parent_dir = os.path.dirname(file)
-        abs_path = os.path.abspath(file)
-        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {file_size} байт, '
-              f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}, Абсолютный путь: {abs_path}')
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M",  time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+
+        print(f'Обнаружен файл: {file} Путь: {filepath} Размер: {filesize} байт Время изменения: {formatted_time}'
+              f' Родительская директория: {parent_dir}')
