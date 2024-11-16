dff936ec4bbbc0503cd0b78f19c82cc5e40aa01d
Sergey <sergastrah@yandex.ru>
2024-11-16 23:28:21 +0400

    My_bot13_4v1

--- module13_4.py
+++ module13_4.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-api = '7591513189:AAEj2y7nsKdUEe-wvBZG14MKwpupfmiF4aI'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
