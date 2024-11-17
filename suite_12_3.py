c41642495c7d5460a86e13daadb11d7516960a54
Sergey <sergastrah@yandex.ru>
2024-11-17 12:42:38 +0400

    test12_3 and suite12_3

---
+++ suite_12_3.py
@@ -0,0 +1,8 @@
+import unittest
+import test_12_3
+
+testST = unittest.TestSuite()
+testST.addTest(unittest.TestLoader().loadTestsFromTestCase(test_12_3.RunnerTest))
+testST.addTest(unittest.TestLoader().loadTestsFromTestCase(test_12_3.TournamentTest))
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(testST)
