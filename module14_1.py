6dd666855af8bae540fc714c4d5a2aa1664d97ac
Sergey <sergastrah@yandex.ru>
2024-11-20 21:02:53 +0400

    SQLite3_1 14_2

--- module14_1.py
+++ module14_1.py
@@ -14,13 +14,15 @@ balance INTEGER NOT NULL
 ''')
 
 for i in range(10):
-    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f"User{i+1}", f"example{i+1}@gmail.com", f"{(i+1)*10}", "1000"))
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f"User{i + 1}",
+                                                                                             f"example{i + 1}@gmail.com",
+                                                                                             f"{(i + 1) * 10}", "1000"))
 
-cursor.execute("UPDATE Users SET balance = ? WHERE id%2 <> ?", (500, 0))
+cursor.execute("UPDATE Users SET balance = ? WHERE id%2 != ?", (500, 0))
 
-cursor.execute("DELETE FROM Users WHERE (id+2)%3 = ?", (0,))
+cursor.execute("DELETE FROM Users WHERE id in (1, 4, 7, 10) != ?", (0,))
 
-cursor.execute("SELECT username, email, age, balance FROM Users WHERE age <> ?", (60,))
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
 users = cursor.fetchall()
 for user in users:
     print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс {user[3]}")
